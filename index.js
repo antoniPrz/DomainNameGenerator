bddee730a63fb3f27bbbbd539f50f6bdd0615d6e
antoni perez <antoni.protoni@gmail.com>
2021-02-22 19:51:27 -0300

    agregado el script al html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let pronoum = ["the","our"]
 let adj = ["great","big","fast","cool","chic"]
 let noun = ["site","web","place","pont","spot"]
 
-
+let arrDomain = [];
 for (let x of pronoum)
 {
 
@@ -13,11 +13,25 @@ for (let x of pronoum)
         for( let a of adj){
             // console.log(a)
                 for(let e of noun){
-                    console.log(x+a+e+".com");
+                    // console.log(x+a+e+".com");
+                    arrDomain.push(x+a+e+".com");
                 }
 
         }
 
-
 }
+
+let myList = document.getElementById("list");
+
+for (let i of arrDomain){
+
+    let li = document.createElement("LI");
+    let text = document.createTextNode(i);
+    li.appendChild(text);
+    myList.appendChild(li);
+}
+
+
+
+
 
